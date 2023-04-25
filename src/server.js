b57185e6cc54d7d72b9672672f51987c7d33234d
rrat1138 <ryan@linkleads.io>
2023-04-24 19:24:17 -0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -16,6 +16,6 @@ const { contractAddress, recipientAddress, tokenAmount, tokenMetadata1 } = req.b
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(8080, () => {
+  console.log('Server started on port 8080');
 });
