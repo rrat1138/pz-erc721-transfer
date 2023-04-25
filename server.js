7d30077693c9364ccafa2815320d10d139eff9ec
rrat1138 <ryan@linkleads.io>
2023-04-24 19:08:15 -0700

    port 8080

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.get('/transfer-tokens/:contractAddress/:recipientAddress/:tokenAmount/:token
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+app.listen(8080, () => {
+  console.log('Server started on port 8080');
 });
