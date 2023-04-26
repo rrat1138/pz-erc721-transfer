b26482cff116f6eff93e8063361ccbf323369332
rrat1138 <ryan@linkleads.io>
2023-04-26 15:48:45 -0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const { transfer } = require('./transferFunction.js');
 
+app.use(bodyParser.json());
 
-app.use(express.json());
-
-// Endpoint for receiving data via query parameters
-app.get('/transfer-tokens/:contractAddress/:recipientAddress/:tokenAmount/:tokenMetadata', async (req, res) => {
-  const { contractAddress, recipientAddress, tokenAmount } = req.params;
-const tokenMetadata = decodeURIComponent(req.params.tokenMetadata);
-const tokenMetadata1 = JSON.parse(tokenMetadata);
+// Endpoint for receiving data via POST request
+app.post('/transfer-tokens', async (req, res) => {
+  const { contractAddress, recipientAddress, tokenAmount, tokenMetadata1 } = req.body;
   try {
     const result = await transfer(tokenAmount, recipientAddress, contractAddress, tokenMetadata1);
     res.send(result);
@@ -17,6 +15,7 @@ const tokenMetadata1 = JSON.parse(tokenMetadata);
     res.status(500).send(error);
   }
 });
+
 
 app.listen(8080, () => {
   console.log('Server started on port 8080');
